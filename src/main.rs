27efb858c517388d9de0d7e84a2642dfc4877c42
Ricardo Kullock <ricardokullock@gmail.com>
2021-09-27 11:24:10 -0300

    Split main to a func

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use rand::Rng;
+use rand::{Rng, thread_rng};
 
 fn main() {
-    let mut rng = rand::thread_rng();
+    mru();
+}
+
+fn mru() -> [f32; 6] {
+    let mut rng = thread_rng();
     let vm = rng.gen_range(-100..100) as f32;
-    let _xi = rng.gen_range(-5000..5000) as f32;
+    let xi = rng.gen_range(-5000..5000) as f32;
     let ti = (rng.gen_range(0..96) as f32)/4.0;
     let dt = (rng.gen_range(4..24) as f32)/4.0;
-    let _tf = ti + dt;
+    let tf = ti + dt;
     let dx = vm*dt;
-    println!("vm = {} = {} / {}",vm,dx,dt)
+    println!("vm = {} = {} / {}",vm,dx,dt);
+    let arr1: [f32; 2] = rng.gen();
+    println!("{:?}", arr1);
+    return [vm, xi, ti, dt, tf, dx]
 }
