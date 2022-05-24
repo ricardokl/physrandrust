63be38e80a78368a158585ba79d599c0280557fc
Ricardo Kullock <ricardokullock@gmail.com>
2022-05-24 14:13:00 +0200

    Split into modules, list and main

--- src/list.rs
+++ src/list.rs
@@ -1,5 +1,18 @@
 use crate::ExMru;
 use std::path::Path;
+use std::fs::File;
+use std::fs::write;
+use std::io::{Write, Error};
+use std::fs::OpenOptions;
+
+fn file_generator(ex: String, out: &Path) -> Result<(), Error> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(out)?;
+    file.write_all(ex.as_bytes())?;
+    Ok(())
+}
 
 pub fn gen_list(x: u8, dir: &Path) {
     for _ in 0..x {
@@ -16,18 +29,17 @@ pub fn completely_randomized_set(x: u8, y:u8, dir: &Path) {
     }
 }
 
-pub fn rand_values_only(x: u8, y: u8, dir: &Path) {
+pub fn rand_values_only(x: u8, y: u8, dir: &Path, out: &Path) {
     let mut ex_set: Vec<ExMru> = vec!();
     for _ in 0..x {
         let ex = ExMru::new(dir, 100, 100, 100);
         ex_set.push(ex);
     }
     for _ in 0..y {
-        println!("{}", "======== Lista ========");
         for ex in ex_set.iter_mut() {
             ex.recalc(70,70,70);
             let new = ex.subs();
-            println!("{}", new)
+            file_generator(new, out).unwrap();
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-#![allow(dead_code)]
 #![allow(unused_imports)]
+#![allow(dead_code)]
 mod exercicios;
 mod list;
 
 use list::rand_values_only;
-use list::gen_list;
-use list::completely_randomized_set;
+// use list::gen_list;
+// use list::completely_randomized_set;
 use exercicios::ExMru;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 fn main() {
     let dir = Path::new("/home/ricardo/Documents/projetos/physrandrust/src/templates/");
+    let out = Path::new("/home/ricardo/Documents/projetos/physrandrust/teste.txt");
     // let dir2 = Path::new("/home/invalid");
-    rand_values_only(2,3,dir);
+    rand_values_only(2,3,dir, out);
 }
